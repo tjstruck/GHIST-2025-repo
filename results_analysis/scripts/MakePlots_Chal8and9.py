4d1dfa1a4ced8a3b6f790e306bf301ffa26fd588
tjstruck <tjstruck@arizona.edu>
2026-03-22 14:47:49 -0700

    Update to enable plotting sweeps for testing data

--- results_analysis/scripts/MakePlots_Chal8and9.py
+++ results_analysis/scripts/MakePlots_Chal8and9.py
@@ -3,10 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-for chal_deets in [('multi_sweep_bgs', 'chal9'), ('multi_sweep', 'chal8')]:
-    chal, chal_num = chal_deets
+chal_type2chrom = {
+    'testing':'21',
+    'final':'15'
+}
+
+for chal_deets in [('multi_sweep_bgs', 'chal9', 'final'), ('multi_sweep', 'chal8', 'final'), ('multi_sweep', 'chal8', 'testing')]:
+    chal, chal_num, chal_type = chal_deets
+    chrom = chal_type2chrom[chal_type]
     # In a BED, the start is the equivalent of 0-based position for SNPs
-    sweeps = [int(ele.split('\t')[1])+1 for ele in open(f"data/groundtruth/GHIST_2025_{chal}_final_goldstandard.bed").readlines()]
+    sweeps = [int(ele.split('\t')[1])+1 for ele in open(f"data/groundtruth/GHIST_2025_{chal}_{chal_type}_goldstandard.bed").readlines()]
 
     os.makedirs(f"results/paper/", exist_ok=True)
 
@@ -15,9 +21,9 @@ for chal_deets in [('multi_sweep_bgs', 'chal9'), ('multi_sweep', 'chal8')]:
 
     # Path to VCF
     if "_bgs" in chal:
-        vcf_fi = "data/vcf/GHIST_2025_multisweep.growth_bg.15.final.vcf.gz"
+        vcf_fi = f"data/vcf/GHIST_2025_multisweep.growth_bg.{chrom}.{chal_type}.vcf.gz"
     else:
-        vcf_fi = "data/vcf/GHIST_2025_multisweep.15.final.vcf.gz"
+        vcf_fi = f"data/vcf/GHIST_2025_multisweep.{chrom}.{chal_type}.vcf.gz"
 
     callset = allel.read_vcf(
         vcf_fi,
@@ -59,6 +65,7 @@ for chal_deets in [('multi_sweep_bgs', 'chal9'), ('multi_sweep', 'chal8')]:
     axs[1].set_ylabel("Tajima's D")
     axs[1].set_xlabel(f"Position on simulated human chromosome {chrom[0]} (bp)")
 
+    # Add figure panel labeling
     for ax, label in zip(axs, ["A", "B"]):
         ax.text(
             -0.145, 1.15, label,          # adjust -0.12 to taste
@@ -73,14 +80,19 @@ for chal_deets in [('multi_sweep_bgs', 'chal9'), ('multi_sweep', 'chal8')]:
             ax.axvline(sweep, color="black", linestyle="--")
     plt.tight_layout()
 
+    # Get submission intervals
     fid = open(f"data/final_submissions/{chal}/{chal}_srong.bed")
     intervals = [(int(line.split()[1]), int(line.split()[2])) for line in fid if line.strip()]
     fid.close()
 
-    for ax in axs:
-        for left, right in intervals:
-            ax.axvspan(left, right, color='green', alpha=0.3)
-    plt.savefig(f"results/paper/2025_{chal_num}.pdf", dpi=150)
+    if chal_type is not 'testing':
+        for ax in axs:
+            for left, right in intervals:
+                ax.axvspan(left, right, color='green', alpha=0.3)
+    if chal_type == 'testing':
+        plt.savefig(f"results/paper/2025_{chal_num}_{chal_type}.pdf", dpi=150)
+    else:
+        plt.savefig(f"results/paper/2025_{chal_num}.pdf", dpi=150)
     plt.show()
 
 
